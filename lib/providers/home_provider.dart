896d22479ccf242eab33057d54ed63e7c43572c4
Ethan Ray <ethan.gregory.ray@gmail.com>
2023-10-18 12:23:49 +0100

    needed changes

--- lib/providers/home_provider.dart
+++ lib/providers/home_provider.dart
@@ -3,10 +3,10 @@ import 'package:provider/provider.dart';
 import 'package:uni_app/state/home_state.dart';
 
 //This is boiler plate code for all providers!
-class FlashCardProvider extends StatelessWidget {
+class HomeProvider extends StatelessWidget {
   final Widget child;
 
-  const FlashCardProvider({super.key, required this.child});
+  const HomeProvider({super.key, required this.child});
 
   @override
   Widget build(BuildContext context) {
